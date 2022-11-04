d160d2e988880ed84366d3c748240e030927c059
AlphaAttang <alphaa.attang@gmail.com>
2022-11-04 07:42:28 +0100

    updated

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,22 +1,30 @@
-from turtle import title
-from unittest.util import _MAX_LENGTH
+# from turtle import title
+from datetime import date, datetime
 from django.db import models
 
 # Create your models here.
-
 class Artiste(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    first_name = models.CharField(max_length= 40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Song(models.Model):
-    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    date_released = models.DateTimeField()
-    artist_id = models.IntegerField()
-    likes = models.IntegerField()
+    title = models.CharField(max_length= 40)
+    date_released = models.DateTimeField(auto_now_add=False)
+    likes = models.IntegerField(default=0)
+    artist_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
 
 class Lyric(models.Model):
-    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    content = models.CharField(max_length=500)
-    song_id = models.IntegerField()
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.content
--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yn=o-1lr-pwy+v=18@a42byj#g@r926-)aqtsq2@gwk6r(y%#@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app created
     'musicapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
 
 ROOT_URLCONF = 'songcrud.urls'
 
