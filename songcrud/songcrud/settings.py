7b351923015a6f83ad645a3572de0c6fe609a65a
AlphaAttang <alphaa.attang@gmail.com>
2022-11-14 23:02:17 +0100

    Added daetime format

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USE_L10N = False
+
+DATE_INPUT_FORMATS = ['%d/%m/%Y']
